7eebf7c3acf92879965724fc31e2910daf318948
Wander Neto <wander.dmneto@gmail.com>
2021-04-09 13:39:54 +1000

    using handlebars

--- src/app.js
+++ src/app.js
@@ -3,8 +3,28 @@ const express = require('express');
 
 const app = express();
 
+app.set('view engine', 'hbs');
 app.use(express.static(path.join(__dirname, '../public')));
 
+app.get('', (req, res) => {
+    res.render('index', {
+        title: 'Weather App',
+        name: 'Neto'
+    });
+})
+
+app.get('/about', (req, res) => {
+    res.render('about', {
+        title: 'About!!',
+    });
+})
+
+app.get('/help', (req, res) => {
+    res.render('help', {
+        title: 'Help Page!!',
+        message: 'This is the help message.'
+    });
+})
  app.get('/weather', (req, res) => {
     res.send({
         location: 'Brisbane',
