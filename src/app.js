a964077c747c3a0571a49dd990b1e3702da608ac
Wander Neto <wander.dmneto@gmail.com>
2021-04-09 10:09:35 +1000

    starting web server

---
+++ src/app.js
@@ -0,0 +1,17 @@
+const path = require('path');
+const express = require('express');
+
+const app = express();
+
+app.use(express.static(path.join(__dirname, '../public')));
+
+ app.get('/weather', (req, res) => {
+    res.send({
+        location: 'Brisbane',
+        forecast: 'Partly cloud'
+    });
+ });
+
+app.listen(3000, () => {
+    console.log('Server is up on port 3000.')
+});
