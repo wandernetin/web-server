dde61ae11ccafe2ea742ebb828330897691b0982
Wander Neto <wander.dmneto@gmail.com>
2021-04-18 11:54:03 +1000

    using partials

--- src/app.js
+++ src/app.js
@@ -1,14 +1,22 @@
 const path = require('path');
 const express = require('express');
+const hbs = require('hbs');
 
 const app = express();
 
-const viewsPath = path.join(__dirname, '../template');
+// Define paths for express template
+const viewsPath = path.join(__dirname, '../template/views');
+const partialsPath = path.join(__dirname, '../template/partials');
 
+// Setup handlebars engine and view locations
 app.set('view engine', 'hbs');
 app.set('views', viewsPath);
+hbs.registerPartials(partialsPath);
+
+// Setup static directory to serve
 app.use(express.static(path.join(__dirname, '../public')));
 
+// Routes
 app.get('', (req, res) => {
     res.render('index', {
         title: 'Weather App',
@@ -19,13 +27,15 @@ app.get('', (req, res) => {
 app.get('/about', (req, res) => {
     res.render('about', {
         title: 'About!!',
+        name: 'Neto'
     });
 })
 
 app.get('/help', (req, res) => {
     res.render('help', {
         title: 'Help Page!!',
-        message: 'This is the help message.'
+        message: 'This is the help message.',
+        name: 'Neto'
     });
 })
  app.get('/weather', (req, res) => {
